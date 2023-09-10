67694627b20cd34a522ec32446c512316a9ca666
wangchenxi <2423512757@qq.com>
2023-09-10 16:32:53 +0800

    feat: 新增获取头像和上传图片接口
    Signed-off-by: wangchenxi <2423512757@qq.com>

---
+++ CISP-client/src/api/image.ts
@@ -0,0 +1,13 @@
+import request from './request'
+import {RequestType, RequestUrl} from '@/types/enum'
+
+// 获取所有的系统自带头像
+export const getAllInitialAvatar = async () => {
+    return await request(RequestType.GET, RequestUrl.Image_GetInitialAll);
+}
+
+// 上传一张图片
+export const upload=async()=>{
+    return await request(RequestType.POST, RequestUrl.Image_UploadOne);
+}
+
--- CISP-server/app.js
+++ CISP-server/app.js
@@ -2,8 +2,9 @@ require('./models');
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const expressSession = require('express-session');
-const { AdminRouter, staticMiddleware, CommentReplyRouter, CommentRouter, FriendRouter, GroupRouter, MessageRouter, NewsRouter, SearchRouter, SearchImgRouter, UnableRouter, UserRouter, CaptchaRouter } = require('./routes');
+const { AdminRouter, CommentReplyRouter, CommentRouter, FriendRouter, GroupRouter, MessageRouter, NewsRouter, SearchRouter, SearchImgRouter, UnableRouter, UserRouter, CaptchaRouter,ImageRouter } = require('./routes');
 const errorMiddleware = require('./middleware/errorMiddleWare');
+const staticMiddleware = require('./middleware/staticMiddleWare');
 const sequelize = require("./sequelize");
 // 对数据库进行测试
 // (async () => {
@@ -32,5 +33,6 @@ app.use('/api/searchImg', SearchImgRouter);
 app.use('/api/unable', UnableRouter);
 app.use('/api/user', UserRouter);
 app.use('/api/captcha', CaptchaRouter);
+app.use('/api/static', ImageRouter);
 app.use(errorMiddleware);
 module.exports = app
