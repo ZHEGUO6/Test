47c5f4a5f159b8da4b1cd86b2def00811cf0cfd0
wangchenxi <2423512757@qq.com>
2023-09-22 12:40:53 +0800

    fix: 格式化遗漏的文件

--- CISP-server/app.js
+++ CISP-server/app.js
@@ -1,38 +1,57 @@
-require('./models');
-const express = require('express');
-const cookieParser = require('cookie-parser');
-const expressSession = require('express-session');
-const { AdminRouter, CommentReplyRouter, CommentRouter, FriendRouter, GroupRouter, MessageRouter, NewsRouter, SearchRouter, SearchImgRouter, UnableRouter, UserRouter, CaptchaRouter,ImageRouter } = require('./routes');
-const errorMiddleware = require('./middleware/errorMiddleWare');
-const staticMiddleware = require('./middleware/staticMiddleWare');
+require("./models");
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const expressSession = require("express-session");
+const {
+  AdminRouter,
+  CommentReplyRouter,
+  CommentRouter,
+  FriendRouter,
+  GroupRouter,
+  MessageRouter,
+  NewsRouter,
+  SearchRouter,
+  SearchImgRouter,
+  UnableRouter,
+  UserRouter,
+  CaptchaRouter,
+  ImageRouter,
+} = require("./routes");
+const errorMiddleware = require("./middleware/errorMiddleWare");
+const staticMiddleware = require("./middleware/staticMiddleWare");
 const sequelize = require("./sequelize");
 // 对数据库进行测试
 // (async () => {
 //     await sequelize.sync({ force: true });
 // })()
-const secret = Date.now().toString(36).slice(4) + Math.random().toString(36).slice(4) + 'CISP-Alen';
+const secret =
+  Date.now().toString(36).slice(4) +
+  Math.random().toString(36).slice(4) +
+  "CISP-Alen";
 
 const app = express();
 app.use(staticMiddleware);
 app.use(cookieParser(secret));
-app.use(expressSession({
+app.use(
+  expressSession({
     rolling: true,
     saveUninitialized: false,
     secret,
     resave: false,
-}))
-app.use('/api/admin', AdminRouter);
-app.use('/api/commentReply', CommentReplyRouter);
-app.use('/api/comment', CommentRouter);
-app.use('/api/friend', FriendRouter);
-app.use('/api/group', GroupRouter);
-app.use('/api/message', MessageRouter);
-app.use('/api/news', NewsRouter);
-app.use('/api/search', SearchRouter);
-app.use('/api/searchImg', SearchImgRouter);
-app.use('/api/unable', UnableRouter);
-app.use('/api/user', UserRouter);
-app.use('/api/captcha', CaptchaRouter);
-app.use('/api/static', ImageRouter);
+  })
+);
+app.use("/api/admin", AdminRouter);
+app.use("/api/commentReply", CommentReplyRouter);
+app.use("/api/comment", CommentRouter);
+app.use("/api/friend", FriendRouter);
+app.use("/api/group", GroupRouter);
+app.use("/api/message", MessageRouter);
+app.use("/api/news", NewsRouter);
+app.use("/api/search", SearchRouter);
+app.use("/api/searchImg", SearchImgRouter);
+app.use("/api/unable", UnableRouter);
+app.use("/api/user", UserRouter);
+app.use("/api/captcha", CaptchaRouter);
+app.use("/api/static", ImageRouter);
 app.use(errorMiddleware);
-module.exports = app
+module.exports = app;
--- CISP-server/sequelize.js
+++ CISP-server/sequelize.js
@@ -1,10 +1,10 @@
-const { Sequelize } = require('sequelize');
+const { Sequelize } = require("sequelize");
 const sequelize = new Sequelize({
-    database: 'CISP',
-    username: 'root',
-    password: 'TrustmeChenxi.1314',
-    dialect: 'mysql',
-    ssl: true,
-})
+  database: "CISP",
+  username: "root",
+  password: "TrustmeChenxi.1314",
+  dialect: "mysql",
+  ssl: true,
+});
 
-module.exports = sequelize
+module.exports = sequelize;
